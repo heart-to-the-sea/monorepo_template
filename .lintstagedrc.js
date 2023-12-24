9bd6b4fb542b2fc81e67f93f0db8d742acd9ef9c
Your Name <you@example.com>
2023-12-24 18:36:10 +0800

    feat(init czrc): init czrc

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,21 +1,25 @@
 const { ESLint } = require("eslint");
 /**
  * 过滤我们需要忽略的文件
- * @param {*} files 
- * @returns 
+ * @param {*} files
+ * @returns
  */
 const removeIgnoredFiles = async (files) => {
   const eslint = new ESLint();
   const ignoredFiles = await Promise.all(
     files.map((file) => eslint.isPathIgnored(file)),
   );
-  const filteredFiles = files.filter((_, i) => ignoredFiles[i]);
+  const filteredFiles = files.filter((_, i) => !ignoredFiles[i]);
+  console.log("需要过滤的文件列表", ignoredFiles,filteredFiles);
+
+
   return filteredFiles.join(" ");
 };
 
 module.exports = {
   "*": async (files) => {
     const filesToLint = await removeIgnoredFiles(files);
+    console.log("文件列表", filesToLint);
     return [`eslint ${filesToLint} --max-warnings=0`];
   },
 };
